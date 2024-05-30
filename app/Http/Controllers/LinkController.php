ec8b25dee50890f0246c4c7af8ada0953a4fc842
quang <quang6217@gmail.com>
2024-05-30 14:45:08 +0700

    Update

--- app/Http/Controllers/LinkController.php
+++ app/Http/Controllers/LinkController.php
@@ -289,10 +289,11 @@ class LinkController extends Controller
                 }
             
                 // Thêm user_id vào mảng user_id của phần tử tương ứng
-                // if (!in_array($user_id, $temp_result[$target_uid_post]['user_id'])) {
-                //     $temp_result[$target_uid_post]['user_id'][] = $user_id;
-                // }
-            
+                if($user_id != ''){
+                    if (!in_array($user_id, $temp_result[$target_uid_post]['user_id'])) {
+                        $temp_result[$target_uid_post]['user_id'][] = $user_id;
+                    }
+                } 
                 // Cập nhật trạng thái và theo dõi
                 if ($status == 1) {
                     $temp_result[$target_uid_post]['status'] = 1;
