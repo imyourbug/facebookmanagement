b1618161b194fc8327ba922d9eed0854a764076f
quang <quang6217@gmail.com>
2024-05-31 15:28:03 +0700

    t

--- public/js/admin/linkscan/index.js
+++ public/js/admin/linkscan/index.js
@@ -57,8 +57,8 @@ $(document).ready(function () {
             },
             {
                 data: function (d) {
-                    return 'QQ';
                     console.log(d)
+                    return 'QQ';
                     //return getListAccountNameByUserLink(d.accounts);
                 },
             },
